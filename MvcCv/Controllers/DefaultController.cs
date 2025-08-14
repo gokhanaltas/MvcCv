df620376052cda7e5e36c6150203d67a86ff2b5f
Gökhan Altaş <gokhansteam34407@gmail.com>
2025-08-14 16:16:44 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHAKKIMDA.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult SosyalMedya()
+        {
+            var sosyalmedya = db.TblSosyalMedya.Where(x => x.Durum == true).ToList();
+            return PartialView(sosyalmedya);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TBLDENEYIMLERIM.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TBLEGITIMLERIM.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TBLYETENEKLERIM.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TBLHOBILERIM.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifikalar = db.TBLSERTIFIKALARIM.ToList();
+            return PartialView(sertifikalar);
+        }
+        [HttpGet]
+        public PartialViewResult İletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult İletisim(TBLILETISIM t)
+        {
+            t.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TBLILETISIM.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }   
+}
---
+++ MvcCv/Controllers/EgitimController.cs
@@ -0,0 +1,63 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+
+    [Authorize]
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<TBLEGITIMLERIM> repo = new
+        GenericRepository<TBLEGITIMLERIM>();
+
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(TBLEGITIMLERIM p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");   
+        }
+        [HttpGet]
+        public ActionResult EgitimDuzenle(int id)
+
+        {
+            var egitim = repo.Find(x => x.ID == id);
+           return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EgitimDuzenle(TBLEGITIMLERIM t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimDuzenle");
+            }
+            var egitim = repo.Find(x => x.ID == t.ID);
+            egitim.Baslik = t.Baslik;
+            egitim.AltBaslik1 = t.AltBaslik1;
+            egitim.AltBaslik2 = t.AltBaslik2;
+            egitim.Tarih = t.Tarih;
+            egitim.GNO = t.GNO;
+            repo.TUpdate(egitim);
+            return RedirectToAction("Index");
+        }
+    }
+}
